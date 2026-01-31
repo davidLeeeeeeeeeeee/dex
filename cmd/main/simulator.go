16b36aa0e040a23279e7f5ac6340b31d56fdb902
DESKTOP-LU57J59\Administrator <example@email.com>
2026-01-31 22:22:38 +0800

    1

--- cmd/main/simulator.go
+++ cmd/main/simulator.go
@@ -293,7 +293,7 @@ func (s *TxSimulator) runOrderScenario() {
 		}
 
 		// 每次生成 10-20 笔订单
-		orderCount := 10 + mrand.Intn(11)
+		orderCount := 10 + mrand.Intn(50)
 		for i := 0; i < orderCount; i++ {
 			// 随机选择一个节点
 			nodeIdx := mrand.Intn(len(s.nodes))
